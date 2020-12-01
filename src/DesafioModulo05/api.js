3a389900117105ed7d2b365c2f55fb4dcc4c6113
ricardodm89 <ricado89@gmailcom>
2020-12-01 20:14:43 -0300

    Desafio 05 - Exercício 03

--- src/DesafioModulo05/api.js
+++ src/DesafioModulo05/api.js
@@ -7,6 +7,7 @@ var listElement = document.querySelector('#app ul');
 function addName(){
  var user = inputElement.value;
  inputElement.value = '';
+ renderLoading();
  minhaPromise(user); 
 };
 
@@ -19,6 +20,7 @@ var minhaPromise = function(user){
         xhr.onreadystatechange = function(){
             if(xhr.readyState === 4){
                 if(xhr.status === 200){
+                    
                     // resolve chama o método .then
                     resolve = (JSON.parse(xhr.responseText)); 
                    
@@ -26,6 +28,7 @@ var minhaPromise = function(user){
                         
                         var li = document.createElement('li');
                         var repos = document.createTextNode(name.name);
+                        
                         li.appendChild(repos);
                         console.log('repos',repos);
                         listElement.appendChild(li);
@@ -34,7 +37,7 @@ var minhaPromise = function(user){
 
                 }else{
                     // resolve chama o método .catch    
-                    reject('Erro na requisição');
+                    reject();
                 }
             }
         }
@@ -42,12 +45,31 @@ var minhaPromise = function(user){
     });
 }
 
+function renderLoading(loading) {
+    listElement.innerHTML = "";
+    var textElement = document.createTextNode('Carregando...');
+    var loadingElement = document.createElement('li');
+    loadingElement.appendChild(textElement);
+    listElement.appendChild(loadingElement);
+  }
+
+function renderError(loading) {
+    listElement.innerHTML = "";
+    var textElement = document.createTextNode('Erro!');
+    var errorElement = document.createElement('li');
+    errorElement.style.color = "#F00";
+    errorElement.appendChild(textElement);
+    listElement.appendChild(errorElement);
+  }                    
+
+
+
 minhaPromise()
     .then(function(response){
         console.log(response);
     })
     .catch(function(error){
-        console.warn(error);
+        renderError();
     });
 
 buttonElement.onclick = addName;
