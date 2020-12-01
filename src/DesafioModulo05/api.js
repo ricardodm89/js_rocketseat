b2805d89cc2b19e20ea05e27d943bdba480f4440
ricardodm89 <ricado89@gmailcom>
2020-12-01 18:36:21 -0300

    Desafio 05 - Exercício 02

--- src/DesafioModulo05/api.js
+++ src/DesafioModulo05/api.js
@@ -7,23 +7,31 @@ var listElement = document.querySelector('#app ul');
 function addName(){
  var user = inputElement.value;
  inputElement.value = '';
- 
- minhaPromise(user);
-    
+ minhaPromise(user); 
 };
 
 var minhaPromise = function(user){
-    return new Promise(function(resolve, reject){
+    return new Promise(function( resolve, reject){
         var xhr = new XMLHttpRequest();
         xhr.open('GET', 'https://api.github.com/users/' + user + '/repos');
-        console.log(user);
         xhr.send(null);
 
         xhr.onreadystatechange = function(){
             if(xhr.readyState === 4){
                 if(xhr.status === 200){
                     // resolve chama o método .then
-                    resolve(JSON.parse(xhr.responseText)); 
+                    resolve = (JSON.parse(xhr.responseText)); 
+                   
+                    resolve.forEach(function(name){
+                        
+                        var li = document.createElement('li');
+                        var repos = document.createTextNode(name.name);
+                        li.appendChild(repos);
+                        console.log('repos',repos);
+                        listElement.appendChild(li);
+                        
+                    })
+
                 }else{
                     // resolve chama o método .catch    
                     reject('Erro na requisição');
@@ -34,6 +42,13 @@ var minhaPromise = function(user){
     });
 }
 
+minhaPromise()
+    .then(function(response){
+        console.log(response);
+    })
+    .catch(function(error){
+        console.warn(error);
+    });
 
 buttonElement.onclick = addName;
 
